e2f055f73a4af6f31e606f5248b680ff16008271
ahhhbeey <64306231+ahhhbeey@users.noreply.github.com>
2020-12-23 16:25:25 +0800

    Add files via upload

---
+++ Ko-Abigail-introtopython.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+185602
+Ko
+3 BS ITE
+
+
+# In[10]:
+
+
+firstnumber = input('Enter first number:')
+secondnumber = input('Enter second number:')
+thirdnumber = input('Enter third number:')
+
+def three_number_average():
+    added = float(firstnumber) + float(secondnumber) + float(thirdnumber)
+    average = added/3
+    print("The average is", average)
+
+
+# In[11]:
+
+
+three_number_average()
+
